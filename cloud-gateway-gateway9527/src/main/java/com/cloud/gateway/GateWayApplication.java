43b31e4a6b2adda63bce63d28a4b4c5e3b26819f
李昭 <31624545+GtHbs@users.noreply.github.com>
2020-04-25 11:00:27 +0800

    commit

---
+++ cloud-gateway-gateway9527/src/main/java/com/cloud/gateway/GateWayApplication.java
@@ -0,0 +1,17 @@
+package com.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author: 李昭
+ * @Date: 2020/4/25 9:44
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class,args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/cloud/gateway/gatewayconfig/GateWayConfig.java
@@ -0,0 +1,25 @@
+package com.cloud.gateway.gatewayconfig;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 手动配置路由规则
+ *
+ * @author: 李昭
+ * @Date: 2020/4/25 10:29
+ */
+@Configuration
+public class GateWayConfig {
+
+    @Bean
+    public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
+        RouteLocatorBuilder.Builder routes = builder.routes();
+        routes.route("config_guonei", r -> r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+        routes.route("config_guoji", r -> r.path("/guoji").uri("http://news.baidu.com/guoji")).build();
+        return routes.build();
+    }
+
+}
